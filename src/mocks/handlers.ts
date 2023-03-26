280721eb27a83059b45e1a13b96c372579ca1577
thsuekfk2 <thsuekfk2@naver.com>
2023-03-26 20:13:01 +0900

    refactor : msw handlers

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,32 +1,43 @@
+import { faker } from "@faker-js/faker";
 import { rest } from "msw";
 
 export const handlers = [
   rest.post("/login", (req, res, ctx) => {
-    return res(
-      ctx.status(200),
-      ctx.json({
-        status: "success",
-      })
-    );
+    const { id, password } = req.body as { id: string; password: string };
+
+    if (id === "user1" && password === "password1") {
+      return res(
+        ctx.status(200),
+        ctx.json({
+          status: "success",
+          accessToken: "123456789!@#",
+        })
+      );
+    } else {
+      return res(
+        ctx.status(401),
+        ctx.json({ message: "Invalid ID or password" })
+      );
+    }
   }),
 
   rest.get("/user", (req, res, ctx) => {
-    const isAuthenticated = sessionStorage.getItem("is-authenticated");
+    const token = req.headers.get("Authorization")?.replace("Bearer ", "");
 
-    if (!isAuthenticated) {
+    if (token !== "123456789!@#") {
+      return res(ctx.status(401), ctx.json({ message: "Unauthorized" }));
+    } else {
       return res(
-        ctx.status(403),
+        ctx.status(200),
         ctx.json({
-          errorMessage: "Not authorized",
+          username: faker.name.firstName() + faker.name.lastName(),
+          userAge: faker.datatype.number({
+            min: 10,
+            max: 50,
+          }),
+          role: "user",
         })
       );
     }
-
-    return res(
-      ctx.status(200),
-      ctx.json({
-        username: "admin",
-      })
-    );
   }),
 ];
